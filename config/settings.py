5a774f0a9d59e608cdfdad3d47dbb0f223306b80
Umar Javed <javedumar507@gmail.com>
2025-01-27 14:38:28 +0500

    postgres configuration

--- config/settings.py
+++ config/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "WS",
+        "USER": "root",
+        "PASSWORD": "1210",
+        "HOST": "localhost",   
+        "PORT": "5433",
     }
 }
 
