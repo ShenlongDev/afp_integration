d7bfab60bd256f8c86a01ad959726a577ae566c3
Umar Javed <javedumar507@gmail.com>
2025-02-01 22:01:12 +0500

    updates

--- requirements/deploy.sh
+++ requirements/deploy.sh
@@ -18,7 +18,11 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+
+
 echo "Restarting Gunicorn service..."
 sudo systemctl restart gunicorn
 
+echo "Starting Gunicorn..."
+/var/www/WS-Insights/gunicorn config.wsgi:application --bind 127.0.0.1:8000 --timeout 300 --workers 2
 echo "Deployment completed successfully!"
