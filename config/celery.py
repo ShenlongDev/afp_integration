768cb33742da6b02667e63ffdb8a032a3528ed88
Umar Javed <javedumar507@gmail.com>
2025-02-24 17:38:05 +0500

    optimized performance

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 from django.conf import settings
+from celery.signals import worker_ready
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -11,3 +12,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.broker_connection_retry_on_startup = True
+
+@worker_ready.connect
+def at_start(sender, **kwargs):
+    """
+    When the worker is ready, send a kickstart task.
+    In this case the 'dispatcher' task is enqueued, which is designed
+    to check for high priority or normal tasks and re-enqueue itself when done.
+    """
+    from core.tasks.general import dispatcher
+    # Optionally add a short countdown (e.g., 5 seconds) to give a clean startup.
+    dispatcher.apply_async(countdown=5)
